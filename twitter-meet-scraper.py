dc1e13d3c78f21e86bf887e1728e451343cfe436
kenjir <35819962+cliffordrichardson@users.noreply.github.com>
2020-03-31 14:15:12 -0700

    Create twitter-meet-scraper.py

---
+++ twitter-meet-scraper.py
@@ -0,0 +1,42 @@
+import discord
+from discord.ext import commands
+import requests
+from lxml import html
+import re
+import json
+from bs4 import BeautifulSoup, SoupStrainer
+import asyncio
+from time import sleep
+
+prefix = "!"
+cleanup = True
+
+bot = commands.Bot(command_prefix=prefix)
+
+@bot.event
+async def on_ready():
+    print("Bot is online")
+    game = discord.Game("!<command>")
+    await bot.change_presence(activity=game)
+
+    while(True):
+        response = requests.get('https://twitter.com/search?q=zoom.us%2Fj%2F&src=typed_query&f=live')
+
+        links = re.findall('https?://[^\s]+', response.content.decode())
+        print(links)
+        sleep(5)
+
+@bot.event
+async def on_guild_join(guild):
+    await guild.create_text_channel("zoom-links")
+    await guild.create_text_channel("meet-links")
+
+async def send_results():
+    pass
+
+
+
+
+
+bot.run("NjkyNjQwNTcwOTQxNjM2NjU5.XnxeBg.V0r_nobs1IuMhKrYpOE6cEBTHVE")
+
